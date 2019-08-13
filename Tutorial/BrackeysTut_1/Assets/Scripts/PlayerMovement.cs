45494b10ccc98bd0af503b77de6795d6b4c51e00
rprice312 <rowan.price94@gmail.com>
2019-08-13 22:18:05 +0200

    rebuild of lvl 1 of tut, plus cleanup of files

---
+++ Tutorial/BrackeysTut_1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody playerRB;
+
+    // Player control forces
+    public float fwdForce = 1000f;
+    public float sideForce = 100f;
+    public float torqueForce = 50f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Level 1 Begin!");
+        playerRB.AddForce(0,0,0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        playerRB.AddForce(0, 0, fwdForce * Time.deltaTime);
+
+        if (Input.GetKey("d") )
+        {
+            playerRB.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            
+        }
+        if (Input.GetKey("a") )
+        {
+            playerRB.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);    
+        }
+        
+        if (Input.GetKey("q") )
+        {
+            playerRB.AddTorque(0, 0, torqueForce * Time.deltaTime, ForceMode.Impulse);
+        }
+        
+        if (Input.GetKey("e") )
+        {
+            playerRB.AddTorque(0, 0, -torqueForce * Time.deltaTime, ForceMode.Impulse);
+        }
+    }
+}
